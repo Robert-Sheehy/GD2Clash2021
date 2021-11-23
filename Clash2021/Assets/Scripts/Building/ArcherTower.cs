40708868367beefc6c2a30950a4c214b5be0d8d9
Jack-Patterson <jackpatterson.10.02@gmail.com>
2021-11-23 09:33:00 +0000

    Archer Tower 23/11

--- Clash2021/Assets/Scripts/Building/ArcherTower.cs
+++ Clash2021/Assets/Scripts/Building/ArcherTower.cs
@@ -5,19 +5,48 @@ using UnityEngine;
 
 public class ArcherTower : Building
 {
-
+    bool isAttacking = false;
 
 
     // Start is called before the first frame update
+    new
+    
     void Start()
     {
-
+    MHP = 250;
+    DPS = 110;
+    base.Start();
+    min_distance_ranged = 0;
+    max_distance_ranged = 30;
+    attack_time_interval = 2;
+    
     }
    
     // Update is called once per frame
+    new
+    
     void Update()
     {
+        if (isAttacking){
 
+        }
+        
+        if (current_state != Building_States.Attacking)
+        {
+            if (isAttacking)
+            {
+                isAttacking = false;
+            }
+        }
+
+        base.Update();
+    }
+
+    internal override void attack(int dmg)
+    {
+        base.attack(dmg);
+
+        isAttacking = true;
     }
 
 }
