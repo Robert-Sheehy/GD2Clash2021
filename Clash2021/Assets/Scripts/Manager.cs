27066741e216966726c28b1a1ade4af530b26dc3
STUDENT Darragh M. Sweeney <Darragh.M.Sweeney@students.ittralee.ie>
2021-12-02 13:12:21 +0000

    Signed-off-by: STUDENT Darragh M. Sweeney <Darragh.M.Sweeney@students.ittralee.ie>

--- Clash2021/Assets/Scripts/Manager.cs
+++ Clash2021/Assets/Scripts/Manager.cs
@@ -217,7 +217,7 @@ public class Manager : MonoBehaviour
         {
 
             float distance = 100000f;
-            Building nearest = null;
+            CharacterScript nearest = null;
 
 
             Building buildingScript = unit as Building;
@@ -226,7 +226,7 @@ public class Manager : MonoBehaviour
                 if (Vector3.Distance(buildingScript.transform.position, next_character.transform.position) < distance)
                 {
                     distance = Vector3.Distance(buildingScript.transform.position, next_character.transform.position);
-                    nearest = buildingScript;
+                    nearest = next_character;
                 }
             }
 
