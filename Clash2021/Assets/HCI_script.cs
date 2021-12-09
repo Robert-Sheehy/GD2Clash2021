fddfe6d344068b7602ff7ec15c1df1abd0e7291e
STUDENT Darragh M. Sweeney <Darragh.M.Sweeney@students.ittralee.ie>
2021-12-09 13:01:19 +0000

    Signed-off-by: STUDENT Darragh M. Sweeney <Darragh.M.Sweeney@students.ittralee.ie>

--- Clash2021/Assets/HCI_script.cs
+++ Clash2021/Assets/HCI_script.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HCI_script : MonoBehaviour
 {
     Vector3 lastMousePosition;
     Manager theManager;
+    private static string UIMessage;
+    Text UIMessageBox;
+    float MessageTimer;
+    float MessageLife = 5;
+    int MessageCount = 0;
+    int MaxMessages = 5;
+
     // Start is called before the first frame update
     void Start()
     {
         theManager = FindObjectOfType<Manager>();
+        UIMessageBox = GameObject.Find("UIMessageDisplay").GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MessageTimer += Time.deltaTime;
+        if (MessageTimer >= MessageLife)
+            DisplayMessage("");
+      // UIMessage = string.Format("{0:G}", Count);
 
         if (Input.GetMouseButton(1))
         {
@@ -39,7 +52,8 @@ public class HCI_script : MonoBehaviour
             {
 
                 theManager.spawn_th_at(hitinfo.point);
-              
+
+                DisplayMessage("Spawning Town Hall");
             }
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
@@ -51,9 +65,35 @@ public class HCI_script : MonoBehaviour
 
                 theManager.spawn_drag_at(hitinfo.point);
 
+                DisplayMessage("Spawning Dragon");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitinfo;
+            if (Physics.Raycast(r, out hitinfo))
+            {
+
+                theManager.spawn_Cannon_at(hitinfo.point);
+
+                DisplayMessage("Spawning Cannon");
             }
         }
 
         lastMousePosition = Input.mousePosition;
+    }
+
+    internal void DisplayMessage(string UIMessage)
+    {
+
+        if (UIMessage != "")
+        {
+            MessageTimer = 0;
+            MessageCount++;
+            
+        }
+
+        UIMessageBox.text = UIMessage;
     }
 }
--- Clash2021/Assets/Scripts/Building/CannonScript.cs
+++ Clash2021/Assets/Scripts/Building/CannonScript.cs
@@ -7,7 +7,7 @@ public class CannonScript :Building, IHealth
 {
     public GameObject CannonBall;
 
-    ParticleSystem muzzle_flash;
+    public ParticleSystem muzzle_flash;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +18,7 @@ public class CannonScript :Building, IHealth
         DPS = 100;
         max_distance_ranged = 20f;
         attack_time_interval = 2.5f;
-        muzzle_flash = GetComponent<ParticleSystem>();
-        muzzle_flash.Play();
-       
+
     }
     new 
     // Update is called once per frame
@@ -31,24 +29,29 @@ public class CannonScript :Building, IHealth
         if (current_state == Unit_States.Attacking && current_target && attack_timer <= 0f && within_range(current_target))
         {
 
-
             GameObject new_CannonBall = Instantiate(CannonBall,
                   transform.position + new Vector3(0,1.5f), Quaternion.identity);
             projectile_script NewCannonballScript = new_CannonBall.GetComponent<projectile_script>();
 
-            NewCannonballScript.setupProjectile(transform.position,current_target.transform.position,0,10,theManager,DPS);
+            NewCannonballScript.setupProjectile(transform.position,current_target.transform.position,0,100,theManager,DPS);
+        }
 
-}
+        if(current_state == Unit_States.Attacking && current_target && attack_timer <= 0.25f && within_range(current_target))
+        {
+            muzzle_flash.gameObject.SetActive(true);
+        }
 
-        if (Input.GetKeyDown(KeyCode.F))
-            attack(5);
+        else
+        {
+            muzzle_flash.gameObject.SetActive(false);
+        }
     }
 
 
     internal override void attack(int dmg)
     {
 
-       // current_target.takeDamage(dmg);
+        current_target.takeDamage(dmg);
         muzzle_flash.Play();
 
     }
